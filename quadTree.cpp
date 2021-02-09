fbbf1edde42c10e9e3ccd6aa2566305c6074df2e
James Dean <j_d592@txstate.edu>
2021-02-09 02:05:03 -0600

    Filled out functions

--- quadTree.cpp
+++ quadTree.cpp
@@ -6,15 +6,242 @@
 
 using namespace std;
 
+int THRESHOLD;
+
 int varianceThreshold();
 
-int arithmeticMean(int[], int, int);
+int arithmeticMean(quadTree);
 
-int arithmeticVariance(int[], int, int, int);
+int arithmeticVariance(quadTree);
+
+struct Point
+{
+    int x;
+    int y;
+    Point(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+    Point()
+    {
+        x = 0;
+        y = 0;
+    }
+};
+
+struct Node
+{
+    Point position;
+    int data;
+    Node(Point _position, int _data)
+    {
+        position = _position;
+        data = _data;
+    }
+    Node()
+    {
+        data = 0;
+    }
+};
+
+class quadTree
+{
+    Point topLeft;
+    Point bottomRight;
+    Node *node;
+
+    quadTree *topLeftQuad;
+    quadTree *topRightQuad;
+    quadTree *bottomLeftQuad;
+    quadTree *bottomRightQuad;
+
+public:
+    quadTree()
+    {
+        topLeft = Point(0, 0);
+        bottomRight = Point(0, 0);
+        node = NULL;
+        topLeftQuad = NULL;
+        topRightQuad = NULL;
+        bottomLeftQuad = NULL;
+        bottomRightQuad = NULL;
+    }
+    quadTree(Point _topLeft, Point _bottomRight)
+    {
+        node = NULL;
+        topLeftQuad = NULL;
+        topRightQuad = NULL;
+        bottomLeftQuad = NULL;
+        bottomRightQuad = NULL;
+        topLeft = _topLeft;
+        bottomRight = _bottomRight;
+    }
+    void insert(Node*);
+    Node* find(Point);
+    bool inContainer(Point);
+};
+
+void quadTree::insert(Node *_node)
+{
+    if (_node == NULL)
+    {
+        return;
+    }
+
+    if (!inContainer(_node->position))
+    {
+        return;
+    }
+
+    if (abs(topLeft.x - bottomRight.x) <= 1 && abs(topLeft.y - bottomRight.y) <= 1)
+    {
+        if (node == NULL)
+        {
+            node = _node;
+        }
+        return;
+    }
+
+    if((topLeft.x + bottomRight.x) / 2 >= _node->position.x)
+    {
+        //TopLeftQuad
+        if ((topLeft.y + bottomRight.y) / 2 >= _node->position.y)
+        {
+            if (topLeftQuad == NULL)
+            {
+                topLeftQuad = new quadTree(Point(topLeft.x, topLeft.y), Point((topLeft.x + bottomRight.x) / 2, (topLeft.y + bottomRight.y) / 2));
+            }
+            topLeftQuad->insert(_node);
+
+            if (arithmeticVariance(topLeftQuad) <= THRESHOLD)
+            {
+                topLeftQuad->node->data = arithmeticMean(topLeftQuad);
+                return;
+            }
+        }
+        //BottomLeftQuad
+        else
+        {
+            if (bottomLeftQuad == NULL)
+            {
+                bottomLeftQuad = new quadTree(Point(topLeft.x, (topLeft.y + bottomRight.y) / 2), Point((topLeft.x + bottomRight.x) / 2, bottomRight.y));
+            }
+            bottomLeftQuad->insert(_node);
+
+            if (arithmeticVariance(bottomLeftQuad) <= THRESHOLD)
+            {
+                bottomLeftQuad->node->data = arithmeticMean(bottomLeftQuad);
+                return;
+            }
+        }
+    }
+    else
+    {
+        //TopRightQuad
+        if ((topLeft.y + bottomRight.y) / 2 >= _node->position.y)
+        {
+            if (topRightQuad == NULL)
+            {
+                topRightQuad = new quadTree(Point((topLeft.x + bottomRight.x) / 2, topLeft.y), Point(bottomRight.x, (topLeft.y + bottomRight.y) / 2));
+            }
+            topRightQuad->insert(_node);
+
+            if (arithmeticVariance(topRightQuad) <= THRESHOLD)
+            {
+                topRightQuad->node->data = arithmeticMean(topRightQuad);
+                return;
+            }
+        }
+        //BottomRightQuad
+        else
+        {
+            if (bottomRightQuad == NULL)
+            {
+                bottomRightQuad = new quadTree(Point((topLeft.x + bottomRight.x) / 2, (topLeft.y + bottomRight.y) / 2), Point(bottomRight.x, bottomRight.y));
+            }
+            bottomRightQuad->insert(_node);
+
+            if (arithmeticVariance(bottomRightQuad) <= THRESHOLD)
+            {
+                bottomRightQuad->node->data = arithmeticMean(bottomRightQuad);
+                return;
+            }
+        }
+    }
+}
+
+Node* quadTree::find(Point point)
+{
+    if (!inContainer(point))
+    {
+        return NULL;
+    }
+
+    if (node != NULL)
+    {
+        return node;
+    }
+
+    if ((topLeft.x + bottomRight.x) / 2 >= point.x)
+    {
+        //TopLeftQuad
+        if ((topLeft.y + bottomRight.y) / 2 >= point.y)
+        {
+            if (topLeftQuad == NULL)
+            {
+                return NULL;
+            }
+            return topLeftQuad->find(point);
+        }
+        //BottomLeftQuad
+        else
+        {
+            if (bottomLeftQuad == NULL)
+            {
+                return NULL;
+            }
+            return bottomLeftQuad->find(point);
+        }
+    }
+    else
+    {
+        //TopRightQuad
+        if ((topLeft.y + bottomRight.y) / 2 >= point.y)
+        {
+            if (topRightQuad == NULL)
+            {
+                return NULL;
+            }
+            return topRightQuad->find(point);
+        }
+        //BottomRightQuad
+        else
+        {
+            if (bottomRightQuad == NULL)
+            {
+                return NULL;
+            }
+            return bottomRightQuad->find(point);
+        }
+    }
+};
+
+bool quadTree::inContainer(Point point)
+{
+    if (point.x >= topLeft.x && point.x <= bottomRight.x && point.y >= topLeft.y && point.y <= bottomRight.y)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main()
 {
-    int threshold = varianceThreshold();
+    THRESHOLD = varianceThreshold();
 
     int row = 0, col = 0, numRows = 0, numCols = 0;
     ifstream infile("baboon.pgma");
@@ -48,19 +275,28 @@ int main()
     int array[numRows][numCols];
 
     // Following lines : data
-    for(row = 0; row < numRows; ++row)
+    for (row = 0; row < numRows; ++row)
     {
         for (col = 0; col < numCols; ++col)
         {
             ss >> array[row][col];
-            out << array[row][col] << "  ";
+        }
+    }
+
+    quadTree image(Point(0, 0), Point(numRows, numCols));
+    Node node(Point(numRows, numCols), array);
+    image.insert(&node);
+
+    for (row = 0; row < numRows; ++row) 
+    {
+        for (col = 0; col < numCols; ++col)
+        {
+            out << image.find(Point(row, col))->data << "  ";
         }
         out << endl;
     }
 
     infile.close();
-
-
     out.close();
 
     return 0;
@@ -68,42 +304,43 @@ int main()
 
 int varianceThreshold()
 {
-    int threshold;
+    int _threshold;
     cout << "Specify Variance Threshold (0-1024): " << endl;
-    cin >> threshold;
-    while (threshold < 0 || threshold > 1024)
+    cin >> _threshold;
+    while (_threshold < 0 || _threshold > 1024)
     {
         cout << "Please try again \n"
             << "Variance Threshold must be an integer between 0 and 1024.\n";
-        cin >> threshold;
+        cin >> _threshold;
     }
-    return threshold;
+    return _threshold;
 }
 
-int arithmeticMean(int arr[][], int rowSize, int colSize)
+int arithmeticMean(quadTree quad)
 {
     int sum = 0;
-    for (int i = 1; i <= rowSize; i++)
+    int quadSize =  (abs(quad.bottomRight.x - quad.topLeft.x)) * (abs(quad.topLeft.y - quad.bottomRight.y));
+
+    for (int i = 1; i <= quadSize; i++)
     {
-        for(int n = 1; n <= colSize; n++)
-        {
-            sum = sum + arr[i][n];
-        }
+        sum = sum + quad.node->data;
+        quad.node++;
     }
-    return (sum / (rowSize * colSize));
+
+    return (sum / quadSize);
 }
 
-int arithmeticVariance(int arr[][], int rowSize, int colSize, int varianceThreshold)
+int arithmeticVariance(quadTree quad)
 {
     int sum, base;
-    int arrSize = rowSize * colSize;
-    for (int i = 1; i <= rowSize; i++)
+    int quadSize =  (abs(quad.bottomRight.x - quad.topLeft.x)) * (abs(quad.topLeft.y - quad.bottomRight.y));
+
+    for (int i = 1; i <= quadSize; i++)
     {
-        for (int n = 1; n <= colSize; n++)
-        {
-            base = arr[i][n] - varianceThreshold;
-            sum = sum + pow(base, 2);
-        }
+        base = quad.node->data - arithmeticMean(quad);
+        sum = sum + pow(base, 2);
+        quad.node++;
     }
-    return (sum / (arrSize - 1));
+
+    return (sum / (quadSize - 1));
 }
